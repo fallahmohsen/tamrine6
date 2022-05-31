1c22e0c2bbb46f0c67fa7a28f0f662cd8662a32c
mohsenfallah <mohsefalllah@gmail.com>
2022-05-31 13:38:06 +0430

    Ability to check emptiness

---
+++ src/com/company/LinkedList.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class LinkedList {
+    public  void isnull(ArrayList<Integer> arrayList){
+        if(arrayList.size()== 0){
+            System.out.println("ArrayList is null");
+        }
+    }
+}
