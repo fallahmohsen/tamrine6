7fe7be3be56aa0cf06dea2dc4b51bb5565018fde
mohsenfallah <mohsefalllah@gmail.com>
2022-05-31 13:46:16 +0430

    remove index of ArrayList

--- src/com/company/LinkedList.java
+++ src/com/company/LinkedList.java
@@ -4,12 +4,13 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class LinkedList {
-    public  void isnull(ArrayList<Integer> arrayList){
-        if(arrayList.size()== 0){
+    public void isnull(ArrayList<Integer> arrayList) {
+        if (arrayList.size() == 0) {
             System.out.println("ArrayList is null");
         }
     }
-    public  void add(ArrayList<Integer> arrayList){
+
+    public void add(ArrayList<Integer> arrayList) {
         Scanner scanner = new Scanner(System.in);
         boolean flag = true;
         while (flag) {
@@ -23,6 +24,22 @@ public class LinkedList {
             System.out.println("2.no");
             int selectedNumber3 = scanner.nextInt();
             if (selectedNumber3 == 2) {
+                flag = false;
+            }
+        }
+    }
+
+    public void remove(ArrayList<Integer> arrayList) {
+        Scanner scanner = new Scanner(System.in);
+        boolean flag = true;
+        while (flag) {
+            System.out.println("enter number of index to remove:");
+            arrayList.remove(scanner.nextInt());
+            System.out.println("try again?");
+            System.out.println("1.yes");
+            System.out.println("2.no");
+            int selectedNumber = scanner.nextInt();
+            if (selectedNumber == 2) {
                 flag = false;
             }
         }
