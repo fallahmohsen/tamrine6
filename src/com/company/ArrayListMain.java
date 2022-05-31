a5191d6752b376e8c698ec15bf72f31b3bbf9da0
mohsenfallah <mohsefalllah@gmail.com>
2022-05-31 13:41:40 +0430

    fix add number Ability

--- src/com/company/ArrayListMain.java
+++ src/com/company/ArrayListMain.java
@@ -5,24 +5,10 @@ import java.util.Scanner;
 
 public class ArrayListMain {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        boolean flag = true;
+
         ArrayList<Integer> arrayList = new ArrayList<>();
         ArrayList<Integer> arrayList1 = new ArrayList<>();
-        while (flag) {
-            System.out.println("enter number :");
-            int selectedNumber = scanner.nextInt();
-            int selectedNumber1 = scanner.nextInt();
-            arrayList.add(selectedNumber);
-            arrayList.add(selectedNumber1);
-            System.out.println("try again?");
-            System.out.println("1.yes");
-            System.out.println("2.no");
-            int selectedNumber3 = scanner.nextInt();
-            if (selectedNumber3 == 2) {
-                flag = false;
-            }
-        }
+
         ClearArrayList(arrayList, arrayList1);
         for (int s : arrayList1
         ) {
--- src/com/company/LinkedList.java
+++ src/com/company/LinkedList.java
@@ -1,11 +1,30 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class LinkedList {
     public  void isnull(ArrayList<Integer> arrayList){
         if(arrayList.size()== 0){
             System.out.println("ArrayList is null");
+        }
+    }
+    public  void add(ArrayList<Integer> arrayList){
+        Scanner scanner = new Scanner(System.in);
+        boolean flag = true;
+        while (flag) {
+            System.out.println("enter number :");
+            int selectedNumber = scanner.nextInt();
+            int selectedNumber1 = scanner.nextInt();
+            arrayList.add(selectedNumber);
+            arrayList.add(selectedNumber1);
+            System.out.println("try again?");
+            System.out.println("1.yes");
+            System.out.println("2.no");
+            int selectedNumber3 = scanner.nextInt();
+            if (selectedNumber3 == 2) {
+                flag = false;
+            }
         }
     }
 }
