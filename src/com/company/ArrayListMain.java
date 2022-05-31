8d1882d83becd8956d7c01b0df781463763c5501
mohsenfallah <mohsefalllah@gmail.com>
2022-05-31 13:30:49 +0430

    remove wrong number ArrayList

--- src/com/company/ArrayListMain.java
+++ src/com/company/ArrayListMain.java
@@ -23,6 +23,29 @@ public class ArrayListMain {
                 flag = false;
             }
         }
+        ClearArrayList(arrayList, arrayList1);
+        for (int s : arrayList1
+        ) {
+            System.out.println(s);
+        }
+    }
+
+    public static void ClearArrayList(ArrayList arrayList, ArrayList arrayList1) {
+        if (arrayList.size() % 2 != 0) {
+            int result = arrayList.size();
+            result--;
+            arrayList.remove(result);
+        }
+        for (int i = 0; i < arrayList.size(); i += 2) {
+
+            if (arrayList.size() == 0) {
+                break;
+            }
+            if ((int) arrayList.get(i) < (int) arrayList.get(i + 1)) {
+                arrayList1.add(arrayList.get(i));
+                arrayList1.add(arrayList.get(i + 1));
+            }
+        }
     }
 }
 
